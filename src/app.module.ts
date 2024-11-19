e5362e5664d11ba09f9c9713e90d9fbb360b4454
cobasguerram <cobasguerram@gmail.com>
2024-11-18 22:52:23 -0400

    cobasguerram: agregando modulos nuevos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ProfilesModule } from './profiles/profiles.module';
 import { RemindersModule } from './reminders/reminders.module';
 import { SectionsModule } from './sections/sections.module';
-import { AuthModule } from './auth/auth.module';
 import { NotifsModule } from './notifs/notifs.module';
-import { DatabaseModule } from './database/database.module';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [UsersModule, ProfilesModule, RemindersModule, SectionsModule, AuthModule, NotifsModule, DatabaseModule],
+  imports: [
+    UsersModule,
+    ProfilesModule,
+    RemindersModule,
+    SectionsModule,
+    AuthModule,
+    NotifsModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
