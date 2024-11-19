34a92a90e6f02f18e606d4b953c614adf0da776a
cobasguerram <cobasguerram@gmail.com>
2024-11-18 22:53:12 -0400

    cobasguerram: agregando cookieParser para trabajar con cookies

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 import * as session from 'express-session';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
+
   app.use(
     session({
       secret: 'secret',
@@ -20,7 +23,7 @@ async function bootstrap() {
       },
     }),
   );
-  
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
