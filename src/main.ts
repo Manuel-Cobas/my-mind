4d9d06bd502ff8bff4a05b2bf51765aa77daebc8
cobasguerram <cobasguerram@gmail.com>
2024-11-22 20:29:39 -0400

    cobasguerram: inicializando passport

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 import * as session from 'express-session';
+import * as passport from 'passport';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
-
   app.use(
     session({
       secret: 'secret',
@@ -23,6 +23,8 @@ async function bootstrap() {
       },
     }),
   );
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   await app.listen(process.env.PORT ?? 3000);
 }
