3c580f708bbbc72dbaf3a25648418356b6eead88
cobasguerram <cobasguerram@gmail.com>
2024-11-25 21:00:46 -0400

    cobasguerram: modificando refreshTokenExtractor para buscar el token en la session y no en una cookie

--- src/auth/jwt-extractors/refresh-token.extractor.ts
+++ src/auth/jwt-extractors/refresh-token.extractor.ts
@@ -3,7 +3,7 @@ import { UnauthorizedException } from '@nestjs/common';
 import type { CustomRequest } from 'types';
 
 export const refreshTokenExtractor = (req: CustomRequest) => {
-  const refreshToken = req.cookies['refresh-token'];
+  const refreshToken = req.session.refreshToken;
   if (!refreshToken) throw new UnauthorizedException();
 
   return refreshToken;
